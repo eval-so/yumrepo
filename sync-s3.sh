e4b31320bc80cc9bbc7c7cb341ae6bab5ff787f5
Ricky Elrod <ricky@elrod.me>
2013-05-05 04:15:41 -0400

    fix s3 gzip encoding, and stop syncing some needless files.

--- sync-s3.sh
+++ sync-s3.sh
@@ -6,8 +6,13 @@ do
     mkdir $f
   fi
   pushd $f/rpms
+    rm -v *debuginfo*.rpm
     rpm --addsign *.rpm
     createrepo .
   popd
+
+  s3cmd del s3://yum.eval.so/$f/rpms/repodata/*
   s3cmd sync $f s3://yum.eval.so
+
+  find $f/rpms/repodata/ -name '*.gz' -exec s3cmd put --add-header="Content-Encoding:gzip" \{\} s3://yum.eval.so/$f/rpms/repodata/ \;
 done
